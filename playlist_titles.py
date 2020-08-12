c8b72960f410675d91cfeefa28cd6378c6aa4583
Luka <luka.pavageau@gmail.com>
2020-08-13 00:27:43 +0200

    added basic stats

--- playlist_titles.py
+++ playlist_titles.py
@@ -97,12 +97,65 @@ def statistfy(p_file):
         print(e)
         return False
 
+    freq_channels = {}
+    artists = {}
+    missing = []
+    sum_views = 0
+    min_views = 1000000000000
+    max_views = 0
+    #lets loop over the videos in the playlist and gather some stats
     for element in final_list :
-        print(f'{element["title"]} /// {element["channel_title"]}')
-    #most frequent channels
-    #average view count
-    #view counts graphs (boxplots ?)
-    #most frequent artists
+        #extracting the artists from the titles (luckily the titles nomenclature is pretty consistent)
+        v_title = re.search("(.*)(( [-–] )|( -- ))(.*)", element["title"])
+        if v_title :
+            artist = v_title.group(1)
+            #splitting the featurings
+            l_artists = re.split(" feat. | feat | ft. | & | and ", artist)
+        else :
+            l_artists = re.split(" - Topic", element["channel_title"])
+
+        for artist in l_artists :
+            if artist not in artists and artist != "":
+                artists[artist] = 1
+            elif artist != "" :
+                artists[artist] += 1
+
+        #counting the views
+        views = int(element["stats"]["viewCount"])
+        sum_views += views
+        if views > max_views :
+            max_views = views
+            max_views_vid = element["title"]
+        if views < min_views :
+            min_views = views
+            min_views_vid = element["title"]
+
+        #counting the channels
+        channel_title = element["channel_title"]
+        if channel_title not in freq_channels :
+            freq_channels[channel_title] = 1
+        else :
+            freq_channels[channel_title] +=1
+
+    artists = sorted(artists.items(), key=lambda item : item[1], reverse=True)
+    i = 0
+    print("\n---------------------------------------\n\nMost frequent artists in the playlist : \n")
+    while artists[i][1] >= 3 :
+        print(f"{str(artists[i][1]).rjust(5)} /// {artists[i][0]}")
+        i += 1
+
+    avg_views = sum_views/len(final_list)
+    print(f"\nAverage view count = {round(avg_views, 1)}\n\
+    Min view count = {str(min_views).ljust(9)}| {min_views_vid}\n\
+    Max view count = {str(max_views).ljust(9)}| {max_views_vid}\n")
+    freq_channels = sorted(freq_channels.items(), key=lambda item : item[1], reverse=True)
+    a = True
+    i = 0
+    print("Channels appearing more than once : ")
+    while freq_channels[i][1] > 1 and i < len(freq_channels)-1 :
+        print(f"{str(freq_channels[i][1]).rjust(5)} /// {freq_channels[i][0]}")
+        i += 1
+
 
 def main():
     if len(sys.argv) <= 1 :
